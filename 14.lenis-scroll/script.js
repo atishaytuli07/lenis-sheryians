e4dbbe7b2cc25474278f10d0dfdedd3bb74824bc
atishaytuli07 <atishaytuliiaf@gmail.com>
2024-09-16 07:50:26 +0100

    lenis-project

---
+++ 14.lenis-scroll/script.js
@@ -0,0 +1,41 @@
+const lenis = new Lenis()
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
+gsap.registerPlugin(ScrollTrigger)
+
+document.querySelectorAll(".elem").forEach(elem => {
+  let img = elem.querySelector("img")
+  let tl = gsap.timeline()
+
+  let xTransform = gsap.utils.random(-100, 100);
+  tl
+  .set(img, {
+    transformOrigin: `${xTransform < 0 ? 0 : '100%'}`
+  }, "start")
+  .to(img, {
+    scale: 0,
+    ease: "none",
+    scrollTrigger: {
+      trigger: img,
+      start: "top top",
+      end: "bottom top",
+      scrub: true,
+    }
+  }, "start")
+  .to(elem, {
+    xPercent: xTransform,
+    ease: "none",
+    scrollTrigger: {
+      trigger: img,
+      start: "top bottom",
+      end: "bottom top",
+      scrub: true,
+    }
+  })
+})
